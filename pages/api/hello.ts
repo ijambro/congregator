a2517d3f68b8c5b3bf43cff59a352d87f06081c2
Jake Palmer <ijambro@gmail.com>
2022-11-22 14:42:54 -0500

    api hello config runtime: "experimental-edge"

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// Cloudflare requires Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+export const config = {
+  runtime: "experimental-edge"
+};
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
