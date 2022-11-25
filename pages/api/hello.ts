9021bf7c4446e029b4275d20c558b0a0aa19f70b
Jake Palmer <ijambro@gmail.com>
2022-11-24 22:57:52 -0500

    Change code and config so api hello uses runtime: "experimental-edge"

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,18 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
+
+// type Data = {
+//   name: string;
+// };
+
+// Traditional Node.js-powered API route
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: "John Doe" });
+// }
+
+import { NextRequest } from "next/server";
 
 // Cloudflare requires Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
-export const config = {
-  runtime: "experimental-edge"
-};
-
-type Data = {
-  name: string;
-};
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req: NextRequest) {
+  return new Response(JSON.stringify({ name: "Cloudflare Worker" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 }
