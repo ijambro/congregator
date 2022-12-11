94111993c71bb85db4372ec636088db845891637
Jake Palmer <ijambro@gmail.com>
2022-12-10 23:32:54 -0500

    Disable experimental-edge runtime

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
+// const webpack = require("webpack");
+
 const nextConfig = {
-  experimental: {
-    runtime: "experimental-edge"
-  },
   reactStrictMode: true,
   swcMinify: true
+  // Cloudflare requires Next.js Edge API Routes to deploy as serverless Cloudflare Functions (Workers)
+  // experimental: {
+  //   runtime: "experimental-edge"
+  // }
+
+  // Attempt to deal with "optional" pg-native require from pg
+  // webpack: config => {
+  //   config.plugins.push(
+  //     new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+  //   );
+  //   return config;
+  // }
 };
 
 module.exports = nextConfig;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,31 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-// type Data = {
-//   name: string;
-// };
+type Data = {
+  name: string;
+};
 
 // Traditional Node.js-powered API route
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   res.status(200).json({ name: "John Doe" });
-// }
-
-import { NextRequest } from "next/server";
-
-// Cloudflare requires Next.js Edge API Routes:
-//  https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/
-//  https://nextjs.org/docs/api-routes/edge-api-routes
-export default async function handler(req: NextRequest) {
-  // const { searchParams } = new URL(req.url);
-  // const email = searchParams.get("email");
-
-  return new Response(JSON.stringify({ name: "Cloudflare Worker" }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ name: "Clyde The Investigator" });
 }
