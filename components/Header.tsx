7c2ecef572127a6e67abea98554cd7cfc5899ec4
Jake Palmer <ijambro@gmail.com>
2023-01-05 22:53:05 -0500

    Prevent header from wrapping between gator emoji and site name

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <div className="w-full bg-pink-600 text-white font-black text-6xl px-12 py-4">
+      <div className="w-full bg-pink-600 text-white font-black text-6xl whitespace-nowrap px-12 py-4">
         🐊 Congre<span className="text-green-500">gator</span>
       </div>
     </header>
